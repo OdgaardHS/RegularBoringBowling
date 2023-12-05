cb6ee7fd762c7240f38df9dba0cd703563598dbc
RasmusKanne <78956812+RasmusKanne@users.noreply.github.com>
2023-12-05 10:05:52 +0100

    Scoreboard TV UI
    A UI set in worldspace on a TV that tells the score of each round and in total

--- RegularBoringBowling/Assets/Scripts/Score.cs
+++ RegularBoringBowling/Assets/Scripts/Score.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
@@ -13,6 +15,13 @@ public class Score : MonoBehaviour
     public static int PinsSinceRoundStart = 0;
     private int CurrentRound = 1;
 
+    public TMP_Text Round1;
+    public TMP_Text Round2;
+    public TMP_Text Round3;
+    public TMP_Text Round4;
+    public TMP_Text Round5;
+    public TMP_Text Total;
+
     // Update is called once per frame
     void Update()
     {
@@ -23,7 +32,7 @@ public class Score : MonoBehaviour
             ThrowsLeft = 2;
         }
 
-        if (PinsSinceRoundStart >= 10 && !haveStriked && ThrowsLeft == 1)
+        if (PinsSinceRoundStart >= 10 && !haveStriked && ThrowsLeft >= 1)
         {
             haveStriked = true;
             Debug.Log("The player have striked!");
@@ -32,6 +41,27 @@ public class Score : MonoBehaviour
 
             ThrowsLeft = 2;
         }
+
+        switch (CurrentRound)
+        {
+            case 1:
+                Round1.text = PinsSinceRoundStart.ToString();
+                break;
+            case 2:
+                Round2.text = PinsSinceRoundStart.ToString();
+                break;
+            case 3:
+                Round3.text = PinsSinceRoundStart.ToString();
+                break;
+            case 4:
+                Round4.text = PinsSinceRoundStart.ToString();
+                break;
+            case 5:
+                Round5.text = PinsSinceRoundStart.ToString();
+                break;
+        }
+
+        Total.text = PlayerScore.ToString();
     }
 
     IEnumerator waitTillReset()
